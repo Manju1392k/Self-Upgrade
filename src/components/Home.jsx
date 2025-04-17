3e9be6ea938ec951296f8696780650b50ed1dc10
Manjunath <sreerammanjunath1392@gmail.com>
2025-04-17 20:00:46 +0530

    Navbar-started

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../App.css'
+
+export default function Home() {
+  return (
+    // Div for Navbar.
+    <div className="navbar">
+      {/* Div for logo. */}
+      <div className="logo">
+        
+      </div>
+    </div>
+  )
+}
